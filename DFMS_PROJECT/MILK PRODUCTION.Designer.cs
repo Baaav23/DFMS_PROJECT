f72cf3fa50377890d360fd715e5117323ddb4fcd
Baaav23 <bavlybadry123@gmail.com>
2023-03-02 20:16:54 +0200

    change total  milk label

--- DFMS_PROJECT/MILK PRODUCTION.Designer.cs
+++ DFMS_PROJECT/MILK PRODUCTION.Designer.cs
@@ -451,9 +451,9 @@
             this.label11.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label11.Location = new System.Drawing.Point(744, 264);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(46, 24);
+            this.label11.Size = new System.Drawing.Size(120, 24);
             this.label11.TabIndex = 50;
-            this.label11.Text = "AGE";
+            this.label11.Text = "TOTAL MILK";
             // 
             // textBox7
             // 
