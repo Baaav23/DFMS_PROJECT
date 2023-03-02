082a07db1d3a7d45bdce6e9550b3b4b6fccbfa91
Baaav23 <bavlybadry123@gmail.com>
2023-03-02 20:16:11 +0200

    change label date

--- DFMS_PROJECT/MILK PRODUCTION.Designer.cs
+++ DFMS_PROJECT/MILK PRODUCTION.Designer.cs
@@ -469,9 +469,9 @@
             this.label12.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label12.Location = new System.Drawing.Point(508, 264);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(111, 24);
+            this.label12.Size = new System.Drawing.Size(59, 24);
             this.label12.TabIndex = 48;
-            this.label12.Text = "COW NAME";
+            this.label12.Text = "DATE";
             // 
             // label8
             // 
