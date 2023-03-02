c44c6e4fde0ad3187ba48231c0c983e94918b7a2
Baaav23 <bavlybadry123@gmail.com>
2023-03-02 19:38:22 +0200

    change color in grid view

--- DFMS_PROJECT/MILK PRODUCTION.Designer.cs
+++ DFMS_PROJECT/MILK PRODUCTION.Designer.cs
@@ -386,6 +386,7 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(364, 501);
             this.dataGridView1.Name = "dataGridView1";
