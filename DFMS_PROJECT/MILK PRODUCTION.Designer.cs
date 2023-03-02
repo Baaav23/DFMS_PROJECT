a41c80fcf28f1e4eba9396aa709c8b1ddcc1d7cf
Baaav23 <bavlybadry123@gmail.com>
2023-03-02 19:37:08 +0200

    change color in milk bage

--- DFMS_PROJECT/MILK PRODUCTION.Designer.cs
+++ DFMS_PROJECT/MILK PRODUCTION.Designer.cs
@@ -592,6 +592,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1358, 805);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button2);
