0e9aa1d1afa9484fe7f251ff7394b458997e5cc5
Baaav23 <bavlybadry123@gmail.com>
2023-03-02 19:36:35 +0200

    change color

--- DFMS_PROJECT/COWS.Designer.cs
+++ DFMS_PROJECT/COWS.Designer.cs
@@ -264,6 +264,7 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(365, 501);
             this.dataGridView1.Name = "dataGridView1";
