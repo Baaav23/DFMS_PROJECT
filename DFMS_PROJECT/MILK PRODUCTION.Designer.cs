a417672a69eb8087eb536e72a81cd2b9c89c69a0
Baaav23 <bavlybadry123@gmail.com>
2023-03-02 19:34:55 +0200

    change size

--- DFMS_PROJECT/MILK PRODUCTION.Designer.cs
+++ DFMS_PROJECT/MILK PRODUCTION.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1358, 805);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MILK_PRODUCTION";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
