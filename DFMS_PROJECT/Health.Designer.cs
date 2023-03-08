ef846c65f8eb6b624b6dfbe8a401776100d161ce
Baaav23 <bavlybadry123@gmail.com>
2023-03-08 17:12:01 +0200

    health report design finish

--- DFMS_PROJECT/Health.Designer.cs
+++ DFMS_PROJECT/Health.Designer.cs
@@ -411,9 +411,9 @@
             this.label13.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.label13.Location = new System.Drawing.Point(730, 443);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(185, 44);
+            this.label13.Size = new System.Drawing.Size(289, 44);
             this.label13.TabIndex = 58;
-            this.label13.Text = "COW LIST";
+            this.label13.Text = "HEALTH REPORT";
             // 
             // dataGridView1
             // 
