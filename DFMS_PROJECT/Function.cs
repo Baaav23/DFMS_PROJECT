3bb61fd254462e1408e3344bcade4e8b91c94f15
Baaav23 <bavlybadry123@gmail.com>
2023-03-13 15:25:23 +0200

    create function class

---
+++ DFMS_PROJECT/Function.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cow_Farm_System
+{
+    class Functions
+    {
+        private SqlConnection Con;
+        private SqlCommand Cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private String ConStr;
+
+        public Functions()
+        {
+            ConStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\bavly\4th_year\second_term\window\DFMS_PROJECT\DairyFarmDb.mdf;Integrated Security=True;Connect Timeout=30";
+            Con = new SqlConnection(ConStr);
+            Cmd = new SqlCommand();
+            Cmd.Connection = Con;
+        }
+
+        public DataTable GetData(String Query)
+        {
+            dt = new DataTable();
+            sda = new SqlDataAdapter(Query, Con);
+            sda.Fill(dt);
+            return dt;
+        }
+
+        public int SetData(String Query)
+        {
+            int Cnt = 0;
+            if (Con.State == ConnectionState.Closed)
+            {
+                Con.Open();
+            }
+            Console.WriteLine(Query);
+            Cmd.CommandText = Query;
+            Cnt = Cmd.ExecuteNonQuery();
+            Con.Close();
+            return Cnt;
+        }
+    }
+}
